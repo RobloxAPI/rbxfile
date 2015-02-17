5875f42a0853492414cd0feace6a9649ae4108cd
Anaminus <Anaminus@users.noreply.github.com>
2015-02-17 12:47:42 -0600

    increased verbosity of chunk errors

--- bin/model.go
+++ bin/model.go
@@ -342,6 +342,7 @@ loop:
 		chunk.SetCompressed(rawChunk.compressed)
 
 		if _, fr.err = chunk.ReadFrom(bytes.NewReader(rawChunk.payload)); fr.err != nil {
+			fr.err = errors.New("chunk " + string(rawChunk.signature[:]) + ": " + fr.err.Error())
 			return fr.end()
 		}
 
@@ -963,6 +964,7 @@ func (c *ChunkProperty) ReadFrom(r io.Reader) (n int64, err error) {
 
 	c.Properties, fr.err = newValue().FromArrayBytes(rawBytes)
 	if fr.err != nil {
+		fr.err = errors.New(fmt.Sprintf("type %s (0x%X): %s", newValue().TypeString(), c.DataType, fr.err.Error()))
 		return fr.end()
 	}
 
