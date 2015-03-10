c514b770f85c516e329637357091f1de0a580718
Anaminus <Anaminus@users.noreply.github.com>
2015-03-10 16:28:47 -0500

    Expand rbxfile package description.

--- file.go
+++ file.go
@@ -8,8 +8,22 @@
 // accessed and manipulated using an API similar to that of Roblox.
 //
 // Each Instance also has a set of "properties". Each property has a specific
-// value of a certain type. Every available type is prefixed with "Value", and
-// implements the Value interface.
+// value of a certain type. Every available type implements the Value
+// interface, and is prefixed with "Value".
+//
+// Root structures can be decoded from and encoded to various formats,
+// including Roblox's native file format. This is done with the Decode and
+// Encode functions. In order to use these functions, one or more formats must
+// be registered. Usually, this occurs as a side-effect of importing a package
+// that implements the format. For example, the "bin" sub-package, which
+// implements Roblox's binary format, registers the formats "rbxl" and "rbxm"
+// when imported:
+//
+//     import _ "github.com/robloxapi/rbxfile/bin"
+//
+// Besides decoding from a format, root structures can also be created
+// manually. The best way to do this is through the "declare" sub-package,
+// which provides an easy way to generate root structures.
 package rbxfile
 
 import (
