3c04d4580a5e1be526b422478ee0cb9e40baacdb
Anaminus <Anaminus@users.noreply.github.com>
2015-03-10 11:32:43 -0500

    Add declare sub-package.

---
+++ declare/declare.go
@@ -0,0 +1,222 @@
+package declare
+
+import (
+	"github.com/robloxapi/rbxfile"
+)
+
+// Root declares a rbxfile.Root. It is a list that contains Instance
+// declarations.
+type Root []instance
+
+func build(dinst instance, refs map[string]*rbxfile.Instance, props map[*rbxfile.Instance][]property) *rbxfile.Instance {
+	inst := rbxfile.NewInstance(dinst.className, nil)
+
+	if dinst.reference != "" {
+		refs[dinst.reference] = inst
+		inst.Reference = []byte(dinst.reference)
+	}
+
+	inst.Properties = make(map[string]rbxfile.Value, len(dinst.properties))
+	props[inst] = dinst.properties
+
+	for _, dchild := range dinst.children {
+		child := build(dchild, refs, props)
+		child.SetParent(inst)
+	}
+
+	return inst
+}
+
+// Declare evaluates the Root declaration, generating instances and property
+// values, setting up the instance hierarchy, and resolving references.
+func (droot Root) Declare() *rbxfile.Root {
+	root := &rbxfile.Root{
+		Instances: make([]*rbxfile.Instance, 0, len(droot)),
+	}
+
+	refs := map[string]*rbxfile.Instance{}
+	props := map[*rbxfile.Instance][]property{}
+
+	for _, dinst := range droot {
+		root.Instances = append(root.Instances, build(dinst, refs, props))
+	}
+
+	for inst, properties := range props {
+		for _, prop := range properties {
+			inst.Properties[prop.name] = prop.typ.value(refs, prop.value)
+		}
+	}
+
+	return root
+}
+
+type element interface {
+	element()
+}
+
+type instance struct {
+	className  string
+	reference  string
+	properties []property
+	children   []instance
+}
+
+func (instance) element() {}
+
+// Declare evaluates the Instance declaration, generating the instance,
+// descendants, and property values, setting up the instance hierarchy, and
+// resolving references.
+func (dinst instance) Declare() *rbxfile.Instance {
+	inst := rbxfile.NewInstance(dinst.className, nil)
+
+	refs := map[string]*rbxfile.Instance{}
+	props := map[*rbxfile.Instance][]property{}
+
+	if dinst.reference != "" {
+		refs[dinst.reference] = inst
+		inst.Reference = []byte(dinst.reference)
+	}
+
+	inst.Properties = make(map[string]rbxfile.Value, len(dinst.properties))
+	props[inst] = dinst.properties
+
+	for _, dchild := range dinst.children {
+		child := build(dchild, refs, props)
+		child.SetParent(inst)
+	}
+
+	for inst, properties := range props {
+		for _, prop := range properties {
+			inst.Properties[prop.name] = prop.typ.value(refs, prop.value)
+		}
+	}
+
+	return inst
+}
+
+// Instance declares a rbxfile.Instance. It defines an instance with a class
+// name, and a series of "elements". An element can be a Property declaration,
+// which defines a property for the instance. An element can also be another
+// Instance declaration, which becomes a child of the instance.
+//
+// An element can also be a "Ref" declaration, which defines a string that can
+// be used to refer to the instance by properties with the Reference value
+// type. This also sets the instance's Reference field.
+func Instance(className string, elements ...element) instance {
+	inst := instance{
+		className: className,
+	}
+
+	for _, e := range elements {
+		switch e := e.(type) {
+		case Ref:
+			inst.reference = string(e)
+		case property:
+			inst.properties = append(inst.properties, e)
+		case instance:
+			inst.children = append(inst.children, e)
+		}
+	}
+
+	return inst
+}
+
+type property struct {
+	name  string
+	typ   Type
+	value []interface{}
+}
+
+func (property) element() {}
+
+// Property declares a property of a rbxfile.Instance. It defines the name of
+// the property, a type corresponding to a rbxfile.Value, and the value of the
+// property.
+//
+// The value may be one or more values of any type, which are asserted to a
+// rbxfile.Value corresponding to the given type. If the value(s) cannot be
+// asserted, then the zero value is returned instead.
+//
+// When the type or field of the type is a number, any number type except for
+// complex numbers may be given as the value.
+//
+// The value may be a single rbxfile.Value that corresponds to the given type
+// (e.g. rbxfile.ValueString for String), in which case the value itself is
+// returned.
+//
+// For a given type, values must be the following:
+//
+// String, BinaryString, ProtectedString, Content: A single string or []byte.
+// Extra values are ignored.
+//
+// Bool: A single bool. Extra values are ignored.
+//
+// Int, Float, Double, BrickColor, Token: A single number. Extra values are
+// ignored.
+//
+// UDim: 2 numbers, corresponding to the Scale and Offset fields.
+//
+// UDim2: 4 numbers, corresponding to the fields X.Scale, X.Offset, Y.Scale,
+// and Y.Offset.
+//
+// Ray: Either 2 rbxfile.ValueVector3s, or 6 numbers. The ValueVector3s
+// correspond to the Origin and Direction fields. Each number corresponds to
+// the X, Y, and Z fields of Origin, then Direction.
+//
+// Faces: 6 bools, corresponding to the fields Right, Top, Back, Left, Bottom,
+// Front.
+//
+// Axes: 3 bools, corresponding to the fields X, Y, and Z.
+//
+// Color3: 3 numbers, corresponding to the fields R, G, and B.
+//
+// Vector2, Vector2int16: 2 numbers, corresponding to the fields X and Y.
+//
+// Vector3, Vector3int16: 3 numbers, corresponding to the fields X, Y, and Z.
+//
+// CFrame: Either 10 or 12 values. When 10 values are given, the first value
+// must be a rbxfile.ValueVector3, which corresponds to the Position field.
+// The remaining 9 values must be numbers, which correspond to the components
+// of the Rotation field. When 12 values are given, all must be numbers, with
+// the first 3 corresponding to the X, Y, and Z fields of the Position field.
+// The remaining 9 numbers correspond to the Rotation field.
+//
+// Reference: A single string, []byte or *rbxfile.Instance. Extra values are
+// ignored. When the value is a string or []byte, the reference is resolved by
+// looking for an instance whose "Ref" declaration is equal to the value.
+func Property(name string, typ Type, value ...interface{}) property {
+	return property{name: name, typ: typ, value: value}
+}
+
+// Declare evaluates the Property declaration. Since the property does not
+// belong to any instance, the name is ignored, and only the value is
+// generated.
+func (prop property) Declare() rbxfile.Value {
+	var refs map[string]*rbxfile.Instance
+	return prop.typ.value(refs, prop.value)
+}
+
+// Ref declares a string that can be used to refer to the Instance under which
+// it was declared. This will also set the instance's Reference field.
+type Ref string
+
+func (Ref) element() {}
+
+func test() *rbxfile.Root {
+	return Root{
+		Instance("Part",
+			Property("Name", String, "BasePlate"),
+			Property("CanCollide", Bool, false),
+			Property("Position", Vector3, 0, 10, 0),
+			Property("Size", Vector3, 4, 4, 4),
+			Instance("CFrameValue", Ref("RBX12345"),
+				Property("Name", String, "Value"),
+				Property("Value", CFrame, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1),
+			),
+			Instance("ObjectValue",
+				Property("Name", String, "Value"),
+				Property("Value", Reference, "RBX12345"),
+			),
+		),
+	}.Declare()
+}
---
+++ declare/type.go
@@ -0,0 +1,456 @@
+package declare
+
+import (
+	"github.com/robloxapi/rbxfile"
+)
+
+// Type corresponds to a rbxfile.Type.
+type Type byte
+
+const (
+	_ Type = iota
+	String
+	BinaryString
+	ProtectedString
+	Content
+	Bool
+	Int
+	Float
+	Double
+	UDim
+	UDim2
+	Ray
+	Faces
+	Axes
+	BrickColor
+	Color3
+	Vector2
+	Vector3
+	CFrame
+	Token
+	Reference
+	Vector3int16
+	Vector2int16
+)
+
+func normInt16(v interface{}) int16 {
+	switch v := v.(type) {
+	case int:
+		return int16(v)
+	case uint:
+		return int16(v)
+	case uint8:
+		return int16(v)
+	case uint16:
+		return int16(v)
+	case uint32:
+		return int16(v)
+	case uint64:
+		return int16(v)
+	case int8:
+		return int16(v)
+	case int16:
+		return int16(v)
+	case int32:
+		return int16(v)
+	case int64:
+		return int16(v)
+	case float32:
+		return int16(v)
+	case float64:
+		return int16(v)
+	}
+
+	return 0
+}
+
+func normInt32(v interface{}) int32 {
+	switch v := v.(type) {
+	case int:
+		return int32(v)
+	case uint:
+		return int32(v)
+	case uint8:
+		return int32(v)
+	case uint16:
+		return int32(v)
+	case uint32:
+		return int32(v)
+	case uint64:
+		return int32(v)
+	case int8:
+		return int32(v)
+	case int16:
+		return int32(v)
+	case int32:
+		return int32(v)
+	case int64:
+		return int32(v)
+	case float32:
+		return int32(v)
+	case float64:
+		return int32(v)
+	}
+
+	return 0
+}
+
+func normUint32(v interface{}) uint32 {
+	switch v := v.(type) {
+	case int:
+		return uint32(v)
+	case uint:
+		return uint32(v)
+	case uint8:
+		return uint32(v)
+	case uint16:
+		return uint32(v)
+	case uint32:
+		return uint32(v)
+	case uint64:
+		return uint32(v)
+	case int8:
+		return uint32(v)
+	case int16:
+		return uint32(v)
+	case int32:
+		return uint32(v)
+	case int64:
+		return uint32(v)
+	case float32:
+		return uint32(v)
+	case float64:
+		return uint32(v)
+	}
+
+	return 0
+}
+
+func normFloat32(v interface{}) float32 {
+	switch v := v.(type) {
+	case int:
+		return float32(v)
+	case uint:
+		return float32(v)
+	case uint8:
+		return float32(v)
+	case uint16:
+		return float32(v)
+	case uint32:
+		return float32(v)
+	case uint64:
+		return float32(v)
+	case int8:
+		return float32(v)
+	case int16:
+		return float32(v)
+	case int32:
+		return float32(v)
+	case int64:
+		return float32(v)
+	case float32:
+		return float32(v)
+	case float64:
+		return float32(v)
+	}
+
+	return 0
+}
+
+func normFloat64(v interface{}) float64 {
+	switch v := v.(type) {
+	case int:
+		return float64(v)
+	case uint:
+		return float64(v)
+	case uint8:
+		return float64(v)
+	case uint16:
+		return float64(v)
+	case uint32:
+		return float64(v)
+	case uint64:
+		return float64(v)
+	case int8:
+		return float64(v)
+	case int16:
+		return float64(v)
+	case int32:
+		return float64(v)
+	case int64:
+		return float64(v)
+	case float32:
+		return float64(v)
+	case float64:
+		return float64(v)
+	}
+
+	return 0
+}
+
+func normBool(v interface{}) bool {
+	vv, _ := v.(bool)
+	return vv
+}
+
+func assertValue(t Type, v interface{}) (value rbxfile.Value, ok bool) {
+	switch t {
+	case String:
+		value, ok = v.(rbxfile.ValueString)
+	case BinaryString:
+		value, ok = v.(rbxfile.ValueBinaryString)
+	case ProtectedString:
+		value, ok = v.(rbxfile.ValueProtectedString)
+	case Content:
+		value, ok = v.(rbxfile.ValueContent)
+	case Bool:
+		value, ok = v.(rbxfile.ValueBool)
+	case Int:
+		value, ok = v.(rbxfile.ValueInt)
+	case Float:
+		value, ok = v.(rbxfile.ValueFloat)
+	case Double:
+		value, ok = v.(rbxfile.ValueDouble)
+	case UDim:
+		value, ok = v.(rbxfile.ValueUDim)
+	case UDim2:
+		value, ok = v.(rbxfile.ValueUDim2)
+	case Ray:
+		value, ok = v.(rbxfile.ValueRay)
+	case Faces:
+		value, ok = v.(rbxfile.ValueFaces)
+	case Axes:
+		value, ok = v.(rbxfile.ValueAxes)
+	case BrickColor:
+		value, ok = v.(rbxfile.ValueBrickColor)
+	case Color3:
+		value, ok = v.(rbxfile.ValueColor3)
+	case Vector2:
+		value, ok = v.(rbxfile.ValueVector2)
+	case Vector3:
+		value, ok = v.(rbxfile.ValueVector3)
+	case CFrame:
+		value, ok = v.(rbxfile.ValueCFrame)
+	case Token:
+		value, ok = v.(rbxfile.ValueToken)
+	case Reference:
+		value, ok = v.(rbxfile.ValueReference)
+	case Vector3int16:
+		value, ok = v.(rbxfile.ValueVector3int16)
+	case Vector2int16:
+		value, ok = v.(rbxfile.ValueVector2int16)
+	}
+	return
+}
+
+func (t Type) value(refs map[string]*rbxfile.Instance, v []interface{}) rbxfile.Value {
+	if len(v) == 0 {
+		goto zero
+	}
+
+	if v, ok := assertValue(t, v[0]); ok {
+		return v
+	}
+
+	switch t {
+	case String:
+		switch v := v[0].(type) {
+		case string:
+			return rbxfile.ValueString(v)
+		case []byte:
+			return rbxfile.ValueString(v)
+		}
+	case BinaryString:
+		switch v := v[0].(type) {
+		case string:
+			return rbxfile.ValueString(v)
+		case []byte:
+			return rbxfile.ValueString(v)
+		}
+	case ProtectedString:
+		switch v := v[0].(type) {
+		case string:
+			return rbxfile.ValueString(v)
+		case []byte:
+			return rbxfile.ValueString(v)
+		}
+	case Content:
+		switch v := v[0].(type) {
+		case string:
+			return rbxfile.ValueString(v)
+		case []byte:
+			return rbxfile.ValueString(v)
+		}
+	case Bool:
+		switch v := v[0].(type) {
+		case bool:
+			return rbxfile.ValueBool(v)
+		}
+	case Int:
+		return rbxfile.ValueInt(normInt32(v[0]))
+	case Float:
+		return rbxfile.ValueFloat(normFloat32(v[0]))
+	case Double:
+		return rbxfile.ValueFloat(normFloat64(v[0]))
+	case UDim:
+		if len(v) == 2 {
+			return rbxfile.ValueUDim{
+				Scale:  normFloat32(v[0]),
+				Offset: normInt32(v[1]),
+			}
+		}
+	case UDim2:
+		if len(v) == 4 {
+			return rbxfile.ValueUDim2{
+				X: rbxfile.ValueUDim{
+					Scale:  normFloat32(v[0]),
+					Offset: normInt32(v[1]),
+				},
+				Y: rbxfile.ValueUDim{
+					Scale:  normFloat32(v[2]),
+					Offset: normInt32(v[3]),
+				},
+			}
+		}
+	case Ray:
+		switch len(v) {
+		case 2:
+			origin, _ := v[0].(rbxfile.ValueVector3)
+			direction, _ := v[1].(rbxfile.ValueVector3)
+			return rbxfile.ValueRay{
+				Origin:    origin,
+				Direction: direction,
+			}
+		case 6:
+			return rbxfile.ValueRay{
+				Origin: rbxfile.ValueVector3{
+					X: normFloat32(v[0]),
+					Y: normFloat32(v[1]),
+					Z: normFloat32(v[2]),
+				},
+				Direction: rbxfile.ValueVector3{
+					X: normFloat32(v[3]),
+					Y: normFloat32(v[4]),
+					Z: normFloat32(v[5]),
+				},
+			}
+		}
+	case Faces:
+		if len(v) == 6 {
+			return rbxfile.ValueFaces{
+				Right:  normBool(v[0]),
+				Top:    normBool(v[1]),
+				Back:   normBool(v[2]),
+				Left:   normBool(v[3]),
+				Bottom: normBool(v[4]),
+				Front:  normBool(v[5]),
+			}
+		}
+	case Axes:
+		if len(v) == 3 {
+			return rbxfile.ValueAxes{
+				X: normBool(v[0]),
+				Y: normBool(v[1]),
+				Z: normBool(v[2]),
+			}
+		}
+	case BrickColor:
+		return rbxfile.ValueBrickColor(normUint32(v[0]))
+	case Color3:
+		if len(v) == 3 {
+			return rbxfile.ValueColor3{
+				R: normFloat32(v[0]),
+				G: normFloat32(v[1]),
+				B: normFloat32(v[2]),
+			}
+		}
+	case Vector2:
+		if len(v) == 2 {
+			return rbxfile.ValueVector2{
+				X: normFloat32(v[0]),
+				Y: normFloat32(v[1]),
+			}
+		}
+	case Vector3:
+		if len(v) == 3 {
+			return rbxfile.ValueVector3{
+				X: normFloat32(v[0]),
+				Y: normFloat32(v[1]),
+				Z: normFloat32(v[2]),
+			}
+		}
+	case CFrame:
+		switch len(v) {
+		case 10:
+			p, _ := v[0].(rbxfile.ValueVector3)
+			return rbxfile.ValueCFrame{
+				Position: p,
+				Rotation: [9]float32{
+					normFloat32(v[0]),
+					normFloat32(v[1]),
+					normFloat32(v[2]),
+					normFloat32(v[3]),
+					normFloat32(v[4]),
+					normFloat32(v[5]),
+					normFloat32(v[6]),
+					normFloat32(v[7]),
+					normFloat32(v[8]),
+				},
+			}
+		case 12:
+			return rbxfile.ValueCFrame{
+				Position: rbxfile.ValueVector3{
+					normFloat32(v[0]),
+					normFloat32(v[1]),
+					normFloat32(v[2]),
+				},
+				Rotation: [9]float32{
+					normFloat32(v[3]),
+					normFloat32(v[4]),
+					normFloat32(v[5]),
+					normFloat32(v[6]),
+					normFloat32(v[7]),
+					normFloat32(v[8]),
+					normFloat32(v[9]),
+					normFloat32(v[10]),
+					normFloat32(v[11]),
+				},
+			}
+		}
+	case Token:
+		return rbxfile.ValueToken(normUint32(v[0]))
+	case Reference:
+		switch v := v[0].(type) {
+		case string:
+			return rbxfile.ValueReference{
+				Instance: refs[v],
+			}
+		case []byte:
+			return rbxfile.ValueReference{
+				Instance: refs[string(v)],
+			}
+		case *rbxfile.Instance:
+			return rbxfile.ValueReference{
+				Instance: v,
+			}
+		}
+	case Vector3int16:
+		if len(v) == 3 {
+			return rbxfile.ValueVector3int16{
+				X: normInt16(v[0]),
+				Y: normInt16(v[1]),
+				Z: normInt16(v[2]),
+			}
+		}
+	case Vector2int16:
+		if len(v) == 2 {
+			return rbxfile.ValueVector2int16{
+				X: normInt16(v[0]),
+				Y: normInt16(v[1]),
+			}
+		}
+	}
+
+zero:
+	return rbxfile.NewValue(rbxfile.Type(t))
+}
