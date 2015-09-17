7acfc0c424d5d81e2d3af5f62e372e6530f9fda3
Anaminus <Anaminus@users.noreply.github.com>
2015-09-17 04:34:55 -0500

    Remove Format system.
    Replaced by bin.Serializer.DecoderXML.

--- file.go
+++ file.go
@@ -27,14 +27,11 @@
 package rbxfile
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"github.com/robloxapi/rbxdump"
 	"github.com/satori/go.uuid"
-	"io"
 )
 
 ////////////////////////////////////////////////////////////////
@@ -301,130 +298,4 @@ func (inst *Instance) Set(property string, value Value) {
 	} else {
 		inst.Properties[property] = value
 	}
-}
-
-////////////////////////////////////////////////////////////////
-
-// Format encodes and decodes a single file format for a data structure.
-type Format interface {
-	// Name returns the name of the format.
-	Name() string
-
-	// Magic returns a magic prefix that identifies the format. The magic
-	// string can contain "?" wildcards that each match any one byte.
-	Magic() string
-
-	// Decode decodes data from r into a data structure. API is an API dump
-	// that can be used while decoding, and may be nil.
-	Decode(r io.Reader, api *rbxdump.API) (root *Root, err error)
-
-	// Encode encodes a data structure into w. API is a Roblox API that can be
-	// used while encoding, and may be nil.
-	Encode(w io.Writer, api *rbxdump.API, root *Root) (err error)
-}
-
-var formats []Format
-
-// RegisterFormat registers a file format for use by Codec.
-func RegisterFormat(format Format) {
-	formats = append(formats, format)
-}
-
-////////////////////////////////////////////////////////////////
-
-var ErrFormat = errors.New("unknown format")
-
-// Codec encodes and decodes Roblox files using registered formats.
-type Codec struct {
-	// API is an API structure that can be used by formats to ensure that data
-	// is encoded and decoded correctly.
-	API *rbxdump.API
-}
-
-func match(magic string, b []byte) bool {
-	for i, c := range b {
-		if magic[i] != c && magic[i] != '?' {
-			return false
-		}
-	}
-	return true
-}
-
-// Decode attempts to determine and decode the format of the underlying data
-// stream in `r` by reading the header. Only registered formats are detected.
-//
-// Returns ErrFormat if the format could not be detected.
-func (c *Codec) Decode(r io.Reader) (root *Root, err error) {
-	format, buf := GuessFormat(r)
-	if format == nil {
-		return nil, ErrFormat
-	}
-	return format.Decode(buf, c.API)
-}
-
-// GuessFormat reads `r` in an attempt to determine the format of the data. It
-// returns a Format, if one was found, as well as a Reader that can be used to
-// fully decode `r`.
-func GuessFormat(r io.Reader) (format Format, br io.Reader) {
-	var buf *bufio.Reader
-	if br, ok := r.(*bufio.Reader); ok {
-		buf = br
-	} else {
-		buf = bufio.NewReader(r)
-	}
-	for _, f := range formats {
-		magic := f.Magic()
-		header, err := buf.Peek(len(magic))
-		if err == nil && match(magic, header) {
-			format = f
-		}
-	}
-	return format, buf
-}
-
-// Encode attempts to encode a data structure to a given format. The fmt
-// argument should match the name given by the format's Name() method. Only
-// registered formats can be encoded to.
-//
-// Returns ErrFormat if the given format is not registered.
-func (c *Codec) Encode(w io.Writer, fmt string, root *Root) (err error) {
-	var format Format
-	for _, f := range formats {
-		if fmt == f.Name() {
-			format = f
-			break
-		}
-	}
-	if format == nil {
-		return ErrFormat
-	}
-
-	return format.Encode(w, c.API, root)
-}
-
-////////////////////////////////////////////////////////////////
-
-// DefaultCodec is the Codec used by Encode and Decode.
-var DefaultCodec = &Codec{}
-
-// RegisterAPI registers an API structure to be used by Encode and Decode.
-func RegisterAPI(api *rbxdump.API) {
-	DefaultCodec.API = api
-}
-
-// Decode attempts to determine and decode the format of the underlying data
-// stream in `r` by reading the header. Only registered formats are detected.
-//
-// Returns ErrFormat if the format could not be detected.
-func Decode(r io.Reader) (root *Root, err error) {
-	return DefaultCodec.Decode(r)
-}
-
-// Encode attempts to encode a data structure to a given format. The fmt
-// argument should match the name given by the format's Name() method. Only
-// registered formats can be encoded to.
-//
-// Returns ErrFormat if the given format is not registered.
-func Encode(w io.Writer, fmt string, root *Root) (err error) {
-	return DefaultCodec.Encode(w, fmt, root)
 }
