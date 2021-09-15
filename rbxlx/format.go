475ab2609cc61f8ee961e902ed287dbec7a65c57
Anaminus <anaminus@protonmail.com>
2021-09-15 09:33:31 -0500

    Refactor rbxlx API.
    - Remove Deserialize and Serialize functions.
    - Remove Serializer type and NewSerializer function.
    - Remove Decoder and Encoder interfaces.
    - Add Decoder and Encoder types.

--- rbxlx/format.go
+++ rbxlx/format.go
@@ -6,96 +6,31 @@ import (
 	"io"
 )
 
-// Decoder decodes a Document to a generic rbxfile.Root structure.
-type Decoder interface {
-	Decode(document *Document) (root *rbxfile.Root, err error)
-}
+type Decoder struct{}
 
-// Encoder encodes a rbxfile.Root structure to a Document.
-type Encoder interface {
-	Encode(root *rbxfile.Root) (document *Document, err error)
-}
-
-// Serializer implements functions that decode and encode directly between
-// byte streams and rbxfile Root structures.
-type Serializer struct {
-	Decoder Decoder
-	Encoder Encoder
-}
-
-// NewSerializer returns a new Serializer with a specified decoder and
-// encoder. If either value is nil, the default RobloxCodec will be used in
-// its place.
-func NewSerializer(d Decoder, e Encoder) Serializer {
-	s := Serializer{
-		Decoder: d,
-		Encoder: e,
-	}
-
-	if d == nil || e == nil {
-		var codec RobloxCodec
-
-		if d == nil {
-			s.Decoder = codec
-		}
-		if e == nil {
-			s.Encoder = codec
-		}
-	}
-
-	return s
-}
-
-// Deserialize decodes data from r into a Root structure using the specified
-// decoder.
-func (s Serializer) Deserialize(r io.Reader) (root *rbxfile.Root, err error) {
-	if s.Decoder == nil {
-		return nil, errors.New("a decoder has not been not specified")
-	}
-
+func (Decoder) Decode(r io.Reader) (root *rbxfile.Root, err error) {
 	document := new(Document)
-
 	if _, err = document.ReadFrom(r); err != nil {
 		return nil, errors.New("error parsing document: " + err.Error())
 	}
-
-	root, err = s.Decoder.Decode(document)
+	codec := RobloxCodec{}
+	root, err = codec.Decode(document)
 	if err != nil {
 		return nil, errors.New("error decoding data: " + err.Error())
 	}
-
 	return root, nil
 }
 
-// Serialize encodes data from a Root structure to w using the specified
-// encoder.
-func (s Serializer) Serialize(w io.Writer, root *rbxfile.Root) (err error) {
-	if s.Encoder == nil {
-		return errors.New("an encoder has not been not specified")
-	}
+type Encoder struct{}
 
-	document, err := s.Encoder.Encode(root)
+func (Encoder) Encode(w io.Writer, root *rbxfile.Root) (err error) {
+	codec := RobloxCodec{}
+	document, err := codec.Encode(root)
 	if err != nil {
 		return errors.New("error encoding data: " + err.Error())
 	}
-
 	if _, err = document.WriteTo(w); err != nil {
 		return errors.New("error encoding format: " + err.Error())
 	}
-
 	return nil
-}
-
-// Deserialize decodes data from r into a Root structure using the default
-// decoder.
-func Deserialize(r io.Reader) (root *rbxfile.Root, err error) {
-	codec := RobloxCodec{}
-	return NewSerializer(codec, codec).Deserialize(r)
-}
-
-// Serialize encodes data from a Root structure to w using the default
-// encoder.
-func Serialize(w io.Writer, root *rbxfile.Root) (err error) {
-	codec := RobloxCodec{}
-	return NewSerializer(codec, codec).Serialize(w, root)
 }
