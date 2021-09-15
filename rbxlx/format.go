5c99bfec0838b8743a1e27e5d98e24a51b134240
Anaminus <anaminus@protonmail.com>
2021-09-15 09:52:27 -0500

    Propagate codec options.

--- rbxlx/format.go
+++ rbxlx/format.go
@@ -8,15 +8,24 @@ import (
 
 // Decoder decodes a stream of bytes into a rbxfile.Root according to the rbxlx
 // format.
-type Decoder struct{}
+type Decoder struct {
+	// DiscardInvalidProperties determines how invalid properties are decoded.
+	// If true, when the parser successfully decodes a property, but fails to
+	// decode its value or a component, then the entire property is discarded.
+	// If false, then as much information as possible is retained; any value or
+	// component that fails will be emitted as the zero value for the type.
+	DiscardInvalidProperties bool
+}
 
 // Decode reads data from r and decodes it into root.
-func (Decoder) Decode(r io.Reader) (root *rbxfile.Root, err error) {
+func (d Decoder) Decode(r io.Reader) (root *rbxfile.Root, err error) {
 	document := new(documentRoot)
 	if _, err = document.ReadFrom(r); err != nil {
 		return nil, errors.New("error parsing document: " + err.Error())
 	}
-	codec := robloxCodec{}
+	codec := robloxCodec{
+		DiscardInvalidProperties: d.DiscardInvalidProperties,
+	}
 	root, err = codec.Decode(document)
 	if err != nil {
 		return nil, errors.New("error decoding data: " + err.Error())
@@ -26,11 +35,27 @@ func (Decoder) Decode(r io.Reader) (root *rbxfile.Root, err error) {
 
 // Encoder encodes a rbxfile.Root into a stream of bytes according to the rbxlx
 // format.
-type Encoder struct{}
+type Encoder struct {
+	// ExcludeReferent determines whether the "referent" attribute should be
+	// excluded from Item tags when encoding.
+	ExcludeReferent bool
+
+	// ExcludeExternal determines whether standard <External> tags should be
+	// excluded from the root tag when encoding.
+	ExcludeExternal bool
+
+	// ExcludeMetadata determines whether <Meta> tags should be excluded while
+	// encoding.
+	ExcludeMetadata bool
+}
 
 // Encode formats root, writing the result to w.
-func (Encoder) Encode(w io.Writer, root *rbxfile.Root) (err error) {
-	codec := robloxCodec{}
+func (e Encoder) Encode(w io.Writer, root *rbxfile.Root) (err error) {
+	codec := robloxCodec{
+		ExcludeReferent: e.ExcludeReferent,
+		ExcludeExternal: e.ExcludeExternal,
+		ExcludeMetadata: e.ExcludeMetadata,
+	}
 	document, err := codec.Encode(root)
 	if err != nil {
 		return errors.New("error encoding data: " + err.Error())
