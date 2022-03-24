bba165ff9bc007669b274bdb5a09392227f3de4b
Anaminus <anaminus@protonmail.com>
2022-03-24 18:06:20 -0500

    Add command to dump binary format.

---
+++ cmd/rbxfile-dump/main.go
@@ -0,0 +1,63 @@
+// The rbxfile-dump command dumps the content of a rbxl/rbxm file.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/robloxapi/rbxfile/rbxl"
+)
+
+const usage = `usage: rbxfile-dump [INPUT] [OUTPUT]
+
+Reads a binary RBXL or RBXM file from INPUT, and dumps a human-readable
+representation of the binary format to OUTPUT.
+
+INPUT and OUTPUT are paths to files. If INPUT is "-" or unspecified, then stdin
+is used. If OUTPUT is "-" or unspecified, then stdout is used. Warnings and
+errors are written to stderr.
+`
+
+func main() {
+	var input io.Reader = os.Stdin
+	var output io.Writer = os.Stdout
+
+	flag.Usage = func() { fmt.Fprintf(flag.CommandLine.Output(), usage) }
+	flag.Parse()
+	args := flag.Args()
+	if len(args) >= 1 && args[0] != "-" {
+		in, err := os.Open(args[0])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, fmt.Errorf("open input: %w", err))
+			return
+		}
+		input = in
+		defer in.Close()
+	}
+	if len(args) >= 2 && args[1] != "-" {
+		out, err := os.Create(args[1])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, fmt.Errorf("create output: %w", err))
+			return
+		}
+		defer out.Close()
+		defer func() {
+			err := out.Sync()
+			if err != nil {
+				fmt.Fprintln(os.Stderr, fmt.Errorf("sync output: %w", err))
+				return
+			}
+		}()
+		output = out
+	}
+
+	warn, err := rbxl.Decoder{}.Dump(output, input)
+	if warn != nil {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("warning: %w", warn))
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Errorf("error: %w", warn))
+	}
+}
